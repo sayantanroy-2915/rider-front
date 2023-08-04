bf8f5787b840256ec81a8d7907fdd3c6ee4eaebd
Sayantan Roy <sayantan020796@gmail.com>
2023-08-04 15:39:29 +0530

    Functonalities β

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,26 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function Login() {
 
-    const [formData, setFormData] = useState({cred:'',pswd:''});
+    const [formData, setFormData] = useState({cred:'',password:''});
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('https://localhost:8080/login', formData);
+            const { token } = res.data;
+            localStorage.setItem('token', token);
+            console.log("Successfully logged in");
+        } catch (error) {
+            console.error(error);
+        }
+    }
     
     return <>
         <div className='p-5 w-1/2 flex flex-col bg-slate-700/60 backdrop-blur-md rounded-xl border-double border-slate-400'>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,24 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
 function SignUp() {
 
-    const [formData, setFormData] = useState({name:'',phone:'',email:'',pswd:'',cnfPswd:''});
+    const [formData, setFormData] = useState({name:'',phone:'',email:'',password:''});
+    
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post('https://localhost:8080/registration', formData);
+            console.log("Successfully signed up");
+        } catch (error) {
+            console.error(error);
+        }
+    }
     
     return <>
         <div className='p-5 w-1/2 flex flex-col bg-slate-700/60 backdrop-blur-md rounded-xl border-double border-slate-400'>
@@ -10,7 +26,6 @@ function SignUp() {
             <input type='tel' id='phone' placeholder='Enter phone number' className='m-5 p-5 rounded-xl bg-slate-800/75 text-slate-400'/>
             <input type='email' id='email' placeholder='Enter e-mail ID' className='m-5 p-5 rounded-xl bg-slate-800/75 text-slate-400'/>
             <input type='password' id='password' placeholder='Enter password' className='m-5 p-5 rounded-xl bg-slate-800/75 text-slate-400'/>
-            <input type='password' id='cnfpassword' placeholder='Confirm password' className='m-5 p-5 rounded-xl bg-slate-800/75 text-slate-400'/>
             <input type='submit' className='m-5 p-5 rounded-xl bg-slate-800/75 text-slate-400' value='Register' />
         </div>
     </>;
