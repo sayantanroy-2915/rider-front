e84034b9384c59bcc258365a1506cf0f3540b4ac
Sayantan Roy <32139604+sayantanroy-2915@users.noreply.github.com>
2023-08-16 18:53:17 +0530

    Navigate to home

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 // import bcrypt from 'bcrypt';
 
 function Login() {
 
     const [form] = Form.useForm();
+    const navigate = useNavigate();
 
     const handleSubmit = () => {
         form.validateFields().then((values) => {
         //    values.password = bcrypt.hashSync(values.password,5);
             axios.post('http://localhost:8080/login',values)
             .then(res => {
-                console.log(res);
+                localStorage.setItem("",res.data);
+                navigate("/home");
+                
             })
             .catch(err => {
                 console.error(err);
